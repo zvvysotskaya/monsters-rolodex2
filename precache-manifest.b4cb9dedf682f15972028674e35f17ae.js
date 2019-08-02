c3cd0ad4d3e2e71e8553c1a43abdf341104ee0c0
zvvysotskaya <zhannavyvy@gmail.com>
2019-08-02 02:31:53 -0400

    Updates

---
+++ precache-manifest.b4cb9dedf682f15972028674e35f17ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aae818f70419edb2de4cf8487b38f94",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "ff2b513158f6f7e68a7d",
+    "url": "/monsters-rolodex2/static/css/main.49248c92.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex2/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ff2b513158f6f7e68a7d",
+    "url": "/monsters-rolodex2/static/js/main.b36a9a13.chunk.js"
+  },
+  {
+    "revision": "98d79948ca630f5a08e3",
+    "url": "/monsters-rolodex2/static/js/runtime~main.ce38e8c5.js"
+  }
+]);
